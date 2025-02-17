632f1bddb37e93a3ed4cce90cd741592819bac55
Vishnu <iam.visnu.bhaskar@gmail.com>
2025-02-17 02:45:06 +0000

    Deleted venv folder

--- env/lib/python3.12/site-packages/semantic_forms/static/semantic_forms/semanticCheckbox.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-  $(".ui.checkbox")
-    .not(".initialized")
-    .not("[name*=__prefix__]")
-    .each(function () {
-      $(this).checkbox();
-      $(this).addClass("initialized");
-    });
-});
